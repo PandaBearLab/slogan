e4efe86364c221d645cf9663ccbd2c52d17e5510
Mr Bear <mrbear1024@gmail.com>
2024-07-30 08:10:14 +0800

    混乱效果

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
